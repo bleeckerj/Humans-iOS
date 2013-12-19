2665c1753d954986b7a1f5192c9d2ff7057380c6
Julian Bleecker <julian@nearfuturelaboratory.com>
2013-12-19 00:36:04 -0800

    User Entity

---
+++ humans/Entities/HuUser.h
@@ -0,0 +1,27 @@
+//
+//  Created by Cocoa JSON Editor
+//  http://www.cocoajsoneditor.com
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class NSArray;
+@class NSArray;
+
+@interface HuUser : NSObject
+
+
+
+@property (strong, nonatomic) NSString *email;
+@property (strong, nonatomic) NSArray *humans;
+@property (strong, nonatomic) NSString *id;
+@property (strong, nonatomic) NSArray *services;
+@property (strong, nonatomic) NSString *username;
+@property (strong, nonatomic) NSDecimalNumber *version;
+
+- (id) initWithJSONDictionary:(NSDictionary *)dic;
+- (void) parseJSONDictionary:(NSDictionary *)dic;
+-(NSDictionary *)dictionary;
+- (NSDictionary *)proxyForJson;
+@end
