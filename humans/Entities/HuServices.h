6a5923376863a6069716b6a8714fd44e6b757cc0
Julian Bleecker <julian@nearfuturelaboratory.com>
2013-12-19 00:38:10 -0800

    Entities for service

---
+++ humans/Entities/HuServices.h
@@ -0,0 +1,21 @@
+//
+//  Created by Cocoa JSON Editor
+//  http://www.cocoajsoneditor.com
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface HuServices : NSObject
+
+
+
+@property (strong, nonatomic) NSString *serviceName;
+@property (strong, nonatomic) NSString *serviceUserID;
+@property (strong, nonatomic) NSString *serviceUsername;
+
+- (id) initWithJSONDictionary:(NSDictionary *)dic;
+- (void) parseJSONDictionary:(NSDictionary *)dic;
+
+@end
