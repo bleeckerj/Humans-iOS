7d72bf5e55c902f6cc17bbb414f834aba25328a9
Julian Bleecker <julian@nearfuturelaboratory.com>
2013-12-22 22:32:34 -0800

    Useful UIImage categories

---
+++ humans/Supporting/UIImage-categories/UIColor+UIColor_LighterDarker.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+UIColor_LighterDarker.h
+//  MGBox Demo
+//
+//  Created by julian on 11/20/12.
+//  Copyright (c) 2012 Big Paua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (UIColor_LighterDarker)
+
+- (UIColor *)lighterColor;
+- (UIColor *)darkerColor;
+
+@end
