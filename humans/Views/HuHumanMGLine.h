f8c324df89b4e0aa6c76f30a623cf5aa8f080ef7
Julian Bleecker <julian@nearfuturelaboratory.com>
2013-12-22 22:36:58 -0800

    Useful

---
+++ humans/Views/HuHumanMGLine.h
@@ -0,0 +1,15 @@
+//
+//  HuHumanMGLine.h
+//  humans
+//
+//  Created by julian on 12/22/13.
+//  Copyright (c) 2013 nearfuturelaboratory. All rights reserved.
+//
+
+#import "MGLine.h"
+
+@interface HuHumanMGLine : MGLine
+
+
+
+@end
---
+++ humans/Views/HuHumanMGLine.m
@@ -0,0 +1,36 @@
+//
+//  HuHumanMGLine.m
+//  humans
+//
+//  Created by julian on 12/22/13.
+//  Copyright (c) 2013 nearfuturelaboratory. All rights reserved.
+//
+
+#import "HuHumanMGLine.h"
+#import "UIImageView+AFNetworking.h"
+#import "HuServiceUser.h"
+#import "HuHuman.h"
+
+@implementation HuHumanMGLine
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
