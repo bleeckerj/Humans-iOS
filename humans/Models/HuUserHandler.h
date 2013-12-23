2813bd9c3263527dbcda01bb54bba10239e6ee15
Julian Bleecker <julian@nearfuturelaboratory.com>
2013-12-22 22:36:44 -0800

    Useful

--- humans/Models/HuUserHandler.h
+++ humans/Models/HuUserHandler.h
@@ -10,15 +10,31 @@
 #import "HuHumansHTTPClient.h"
 #import "defines.h"
 #import "HuUser.h"
-@interface HuUserHandler : NSObject
+#import "HuHuman.h"
+
+#import "TwitterStatus.h"
+#import "TwitterUser.h"
+
+#import "InstagramStatus.h"
+#import "InstagramCaption.h"
+#import "InstagramUser.h"
+#import "TransientInstagramUser.h"
+#import "InstagramImages.h"
+#import "InstagramCounts.h"
+#import "InstagramImage.h"
+
+@interface HuUserHandler : NSObject <NSURLConnectionDelegate>
 
 @property (nonatomic, retain) HuHumansHTTPClient *client;
 @property (nonatomic, copy) NSString *access_token;
 @property (nonatomic, copy) HuUser *humans_user;
+@property (nonatomic, readonly) NSMutableDictionary *statusForHumanId;
 
 - (void)userGetWithCompletionHandler:(CompletionHandlerWithResult)completionHandler;
 - (void)userRequestTokenForUsername:(NSString *)username forPassword:(NSString *)password withCompletionHandler:(CompletionHandlerWithResult)completionHandler;
 - (BOOL)usernameExists:(NSString *)username;
 - (void)userFriendsGet:(ArrayOfResultsHandler)completionHandler;
+- (void)getStatusForHuman:(HuHuman *)aHuman withCompletionHandler:(CompletionHandlerWithResult)completionHandler;
+- (NSArray *)statusForHuman:(HuHuman *)aHuman;
 
 @end
--- humans/Models/HuUserHandler.m
+++ humans/Models/HuUserHandler.m
@@ -18,13 +18,13 @@
 #import "HuServices.h"
 #import "HuOnBehalfOf.h"
 #import "HuServiceUser.h"
+#import "HuAppDelegate.h"
 
 @implementation HuUserHandler
 
-
 @synthesize client;
 @synthesize humans_user;
-
+@synthesize statusForHumanId;
 
 - (id)init
 {
@@ -36,9 +36,26 @@
     
 }
 
+- (NSArray *)statusForHuman:(HuHuman *)aHuman
+{
+    __block id obj =  [[self statusForHumanId]objectForKey:[aHuman humanid]];
+    if(obj == nil) {
+        [self getStatusForHuman:aHuman withCompletionHandler:^(BOOL success, NSError *error) {
+            //
+            obj =  [[self statusForHumanId]objectForKey:[aHuman humanid]];
+        }];
+    }
+    return obj;
+}
+
 - (void)commonInit
 {
+    statusForHumanId = [[NSMutableDictionary alloc]init];
     client = [HuHumansHTTPClient sharedDevClient];
+    
+    //LOG_GENERAL(0, @"allowss invalid ssl cert? %@", [client allowsInvalidSSLCertificate]?@"YES":@"NO");
+    
+    
     [client setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
         if (status == AFNetworkReachabilityStatusNotReachable) {
             // Not reachable
@@ -66,27 +83,27 @@
     //[request setHTTPBody:postData];
     [request setValue:[NSString stringWithFormat:@"application/json"] forHTTPHeaderField:@"Accept"];
     [request setValue:[NSString stringWithFormat:@"application/x-www-form-urlencoded"] forHTTPHeaderField:@"Content-Type"];
-    LOG_GENERAL(0, @"%@", request);
+    LOG_GENERAL(0, @"The request is to %@", [request URL]);
     AFJSONRequestOperation *operation = [[AFJSONRequestOperation alloc] initWithRequest:request];
+    
+    
     [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
         //
         LOG_NETWORK(0, @"Success %@", responseObject);
         [self setAccess_token:[responseObject objectForKey:@"access_token"]];
         [self userGetWithCompletionHandler:completionHandler];
         
-//        if(completionHandler) {
-//            completionHandler(true, nil);
-//        }
-        //NSString * result = [responseObject objectForKey:@"result"];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         //
         LOG_NETWORK(0, @"Failure %@", error);
         [self setAccess_token:nil];
-//        if(completionHandler) {
-//            completionHandler(false, error);
-//        }
     }];
     
+    
+    
+    
+    [operation setSSLPinningMode:AFSSLPinningModeNone];
+    //[operation setAllowsInvalidSSLCertificate:YES];
     [operation start];
     [operation waitUntilFinished];
     
@@ -142,7 +159,7 @@
     RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:userMapping method:RKRequestMethodGET pathPattern:nil keyPath:@"" statusCodes:statusCodes];
     
     [objectManager addResponseDescriptor:responseDescriptor];
-
+    
     RKObjectMapping *errorMapping = [RKObjectMapping mappingForClass:[RKErrorMessage class]];
     
     [errorMapping addPropertyMapping:[RKAttributeMapping attributeMappingFromKeyPath:nil toKeyPath:@"errorMessage"]];
@@ -153,20 +170,20 @@
                                             pathPattern:nil
                                                 keyPath:@"message"
                                             statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassClientError)];
-
+    
     
     [objectManager addResponseDescriptor:errorDescriptor];
     
     NSDictionary *queryParams;
     queryParams = [NSDictionary dictionaryWithObjectsAndKeys:[self access_token], @"access_token", nil];
     
-//
     RKObjectRequestOperation *operation;
     operation = [objectManager appropriateObjectRequestOperationWithObject:self method:RKRequestMethodGET path:@"/rest/user/get" parameters:queryParams];
     [operation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
         //
         LOG_NETWORK(0, @"User Get Success %@", [mappingResult firstObject]);
         humans_user = [mappingResult firstObject];
+        
         LOG_GENERAL(0, @"%@", [humans_user dictionary]);
         if(completionHandler) {
             completionHandler(true, nil);
@@ -179,23 +196,13 @@
         if(completionHandler) {
             completionHandler(false, error);
         }
-
+        
     }];
     
     [operation start];
     [operation waitUntilFinished];
     
-
-/*
-    [objectManager getObjectsAtPath:@"/rest/user/get" parameters:queryParams success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
-        LOG_NETWORK(0, @"Success %@", [mappingResult firstObject]);
-        humans_user = [mappingResult firstObject];
-    } failure:^(RKObjectRequestOperation *operation, NSError *error) {
-        RKErrorMessage *errorMessage =  [[error.userInfo objectForKey:RKObjectMapperErrorObjectsKey] firstObject];
-        LOG_NETWORK(0, @"Failed.. %@", error);
-        LOG_NETWORK(0, @"%@", errorMessage);
-    }];
-*/
+    
 }
 
 - (void)userFriendsGet:(ArrayOfResultsHandler)completionHandler
@@ -234,19 +241,6 @@
          LOG_UI_VERBOSE(0, @"success: mappings %@", mappingResult);
          NSMutableArray *results = [NSMutableArray new];
          for (HuFriend *item in [mappingResult array]) {
-             /**
-              UIImageView *iv = [UIImageView new];
-              [iv setImageWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[item largeImageURL]]] placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
-              //
-              //CGImageRef imageRef = [image CGImage];
-              //LOG_INSTAGRAM_IMAGE(0, CGImageGetWidth([image CGImage]), CGImageGetHeight([image CGImage]), UIImageJPEGRepresentation(image, 1.0));
-              [item setLargeProfileImage:image];
-              
-              } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
-              //
-              LOG_ERROR(0, @"Error %@", error);
-              }];
-              **/
              [results addObject:item];
          }
          if(completionHandler) {
@@ -254,7 +248,7 @@
          }
      }
      
-     failure:^(RKObjectRequestOperation * operaton, NSError * error)
+                            failure:^(RKObjectRequestOperation * operaton, NSError * error)
      {
          LOG_NETWORK(0, @"failure: operation: %@ \n\nerror: %@", operaton, error);
          LOG_NETWORK(0, @"errorMessage: %@", [[error userInfo] objectForKey:RKObjectMapperErrorObjectsKey]);
@@ -263,6 +257,143 @@
          }
      }];
     
+}
+
+
+- (void)getStatusForHuman:(HuHuman *)aHuman withCompletionHandler:(CompletionHandlerWithResult)completionHandler
+{
+    RKDynamicMapping *dynamicMapping = [RKDynamicMapping new];
+    
+    RKObjectManager *objectManager = [[RKObjectManager alloc] initWithHTTPClient:client];
+    
+    
+    [dynamicMapping addMatcher:[self twitterStatusMatcher]];
+    [dynamicMapping addMatcher:[self instagramStatusMatcher]];
+    
+    // Success
+    RKResponseDescriptor *dynamicResponseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:dynamicMapping method:RKRequestMethodAny pathPattern:nil keyPath:@"status" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];
+    
+    [objectManager addResponseDescriptor:dynamicResponseDescriptor];
+
+    // Errors
+    RKObjectMapping *errorMapping = [RKObjectMapping mappingForClass:[RKErrorMessage class]];
+
+    RKResponseDescriptor *errorDescriptor =
+    [RKResponseDescriptor responseDescriptorWithMapping:errorMapping
+                                                 method:RKRequestMethodPOST
+                                            pathPattern:nil
+                                                keyPath:@"message"
+                                            statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassClientError)];
+    
+    [objectManager addResponseDescriptor:errorDescriptor];
+
+    
+    NSDictionary *queryParams;
+    queryParams = [NSDictionary dictionaryWithObjectsAndKeys:[self access_token], @"access_token", [aHuman humanid], @"humanid", @"1", @"page", nil];
+    
+    //__block NSArray *array;
+    
+    [objectManager getObjectsAtPath:@"/rest/human/status"
+                         parameters:queryParams
+      success:^(RKObjectRequestOperation * operaton, RKMappingResult *mappingResult)
+     {
+         
+        // NSArray *result = [mappingResult array];
+         LOG_GENERAL(0, @"success: mappings: %@", [mappingResult array]);
+         [[self statusForHumanId] setObject:[mappingResult array] forKey:[aHuman humanid]];
+         if(completionHandler) {
+             completionHandler(true, nil);
+         }
+    }
+      failure:^(RKObjectRequestOperation * operaton, NSError * error)
+     {
+         LOG_GENERAL (0, @"failure: operation: %@ \n\nerror: %@", operaton, error);
+         if(completionHandler) {
+             completionHandler(false, error);
+         }
+     }];
+    
+    
+    
+}
+
+- (RKObjectMappingMatcher *)twitterStatusMatcher
+{
+    RKObjectMapping *twitterStatusMapping = [RKObjectMapping mappingForClass:[TwitterStatus class]];
+    [twitterStatusMapping addAttributeMappingsFromDictionary:@{
+                                                               @"id" : @"tweet_id",
+                                                               @"text" : @"text",
+                                                               //@"user" : @"user",
+                                                               @"created_at" : @"created_at",
+                                                               @"service" : @"name"
+                                                               }];
+    //twitterUserMapping.dateFormatters = [NSArray arrayWithObject: dateFormatter];
+
+    
+    RKObjectMapping *twitterUserMapping = [RKObjectMapping mappingForClass:[TwitterUser class]];
+    [twitterUserMapping addAttributeMappingsFromArray:@[@"id", @"lastUpdated", @"created_at", @"description", @"favourites_count", @"following", @"followers_count", @"friends_count", @"geo_enabled", @"location", @"name", @"screen_name", @"time_zone", @"url", @"utc_offset", @"verified"]];
+    
+    
+    
+    [twitterStatusMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"user"
+                                                                                         toKeyPath:@"user"
+                                                                                       withMapping:twitterUserMapping]];
+    
+    RKObjectMappingMatcher *twitterStatusMatcher = [RKObjectMappingMatcher matcherWithKeyPath:@"service" expectedValue:@"twitter" objectMapping:twitterStatusMapping];
+    
+    
+    return twitterStatusMatcher;
+}
+
+
+- (RKObjectMappingMatcher *)instagramStatusMatcher
+{
+    RKObjectMapping *instagramStatusMapping = [RKObjectMapping mappingForClass:[InstagramStatus class]];
+    [instagramStatusMapping addAttributeMappingsFromDictionary:@{@"id": @"instagram_id",
+                                                                 //@"caption", @"caption",
+                                                                 @"created_time": @"created_time",
+                                                                 @"service" : @"service"
+                                                                 }];
+    
+    RKObjectMapping *instagramCaptionMapping = [RKObjectMapping mappingForClass:[InstagramCaption class]];
+    [instagramCaptionMapping addAttributeMappingsFromArray:@[@"created_time", @"id", @"text"]];
+    
+    RKObjectMapping *instagramCaptionUserMapping = [RKObjectMapping mappingForClass:[InstagramUser class]];
+    [instagramCaptionUserMapping addAttributeMappingsFromArray:@[@"id",@"profile_picture", @"username", @"full_name"]];
+    
+    [instagramCaptionMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"from" toKeyPath:@"from" withMapping:instagramCaptionUserMapping]];
+    
+    [instagramStatusMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"caption" toKeyPath:@"caption" withMapping:instagramCaptionMapping]];
+    
+    RKObjectMapping *instagramImagesMapping = [RKObjectMapping mappingForClass:[InstagramImages class]];
+    //[instagramImagesMapping addAttributeMappingsFromArray:@[@"low_resolution", @"standard_resolution", @"thumbnail"]];
+    
+    
+    RKObjectMapping *instagramImageMapping = [RKObjectMapping mappingForClass:[InstagramImage class]];
+    [instagramImageMapping addAttributeMappingsFromArray:@[@"height", @"url", @"width"]];
+    
+    [instagramImagesMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"low_resolution" toKeyPath:@"low_resolution" withMapping:instagramImageMapping]];
+    [instagramImagesMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"standard_resolution" toKeyPath:@"standard_resolution" withMapping:instagramImageMapping]];
+    [instagramImagesMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"thumbnail" toKeyPath:@"thumbnail" withMapping:instagramImageMapping]];
+    
+    
+    
+    [instagramStatusMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"images" toKeyPath:@"images" withMapping:instagramImagesMapping]];
+  
+    RKObjectMapping *instagramTransientUserMapping = [RKObjectMapping mappingForClass:[TransientInstagramUser class]];
+    [instagramTransientUserMapping addAttributeMappingsFromArray:@[@"bio", @"full_name", @"id", @"lastUpdated", @"profile_picture", @"username", @"website", @"version"]];
+    
+    
+    RKObjectMapping *instagramCountsMapping = [RKObjectMapping mappingForClass:[InstagramCounts class]];
+    [instagramCountsMapping addAttributeMappingsFromArray:@[@"followed_by", @"follows", @"media"]];
+    
+    [instagramTransientUserMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"counts" toKeyPath:@"counts" withMapping:instagramCountsMapping]];
+    
+    [instagramStatusMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"transient_instagram_user" toKeyPath:@"transient_instagram_user" withMapping:instagramTransientUserMapping]];
+
+    RKObjectMappingMatcher *instagramStatusMatcher = [RKObjectMappingMatcher matcherWithKeyPath:@"service" expectedValue:@"instagram" objectMapping:instagramStatusMapping];
+    
+    return instagramStatusMatcher;
 }
 
 - (BOOL)usernameExists:(NSString *)username
@@ -300,5 +431,17 @@
     return exists;
 }
 
+#pragma mark NSURLConnectionDelegate methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+    NSArray *trustedHosts = [NSArray arrayWithObjects:@"localhost",nil];
+    
+    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]){
+        if ([trustedHosts containsObject:challenge.protectionSpace.host]) {
+            [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
+        }
+    }
+    [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
+}
 
 @end
